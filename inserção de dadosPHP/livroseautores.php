4a3f2fd88d44f03778434c77637a0186b83b089e
jcandradee2005 <141359978+jcandradee2005@users.noreply.github.com>
2023-12-05 14:34:15 -0300

    Update livroseautores.php

--- inserção de dadosPHP/livroseautores.php
+++ inserção de dadosPHP/livroseautores.php
@@ -1,67 +1,80 @@
 <?php
-// nome: Maxwell
-$servername = "seu_servidor_mysql";
-$username = "seu_usuario_mysql";
-$password = "sua_senha_mysql";
-$dbname = "seu_banco_de_dados";
-
 // Conexão com o banco de dados
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
 if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// Inserção de dados de autores
-$sql = "INSERT INTO autores (id_autor, nome_autor) VALUES
-    (1, 'Autor 1'),
-    (2, 'Autor 2'),
-    (3, 'Autor 3')";
+// Verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Inserir dados do livro na tabela 'livros'
+    $titulo = $_POST["titulo"];
+    $ano_publicacao = $_POST["ano_publicacao"];
 
-if ($conn->multi_query($sql) === TRUE) {
-    echo "Dados de autores inseridos com sucesso.<br>";
-} else {
-    echo "Erro ao inserir dados de autores: " . $conn->error;
+    $sql = "INSERT INTO livros (titulo, ano_publicacao) VALUES ('$titulo', '$ano_publicacao')";
+
+    if ($conn->query($sql) === TRUE) {
+        $id_livro = $conn->insert_id;
+        echo "Livro inserido com sucesso. ID: " . $id_livro;
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Inserir dados do autor na tabela 'autores'
+    $nome_autor = $_POST["nome_autor"];
+
+    $sql = "SELECT id_autor FROM autores WHERE nome_autor = '$nome_autor'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $id_autor = $row["id_autor"];
+    } else {
+        $sql = "INSERT INTO autores (nome_autor) VALUES ('$nome_autor')";
+
+        if ($conn->query($sql) === TRUE) {
+            $id_autor = $conn->insert_id;
+            echo "Autor inserido com sucesso. ID: " . $id_autor;
+        } else {
+            echo "Erro: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    // Vincular o livro ao autor na tabela 'livros_autores'
+    $sql = "INSERT INTO livros_autores (id_livro, id_autor) VALUES ('$id_livro', '$id_autor')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Livro " . $id_livro . " vinculado ao autor " . $id_autor . " com sucesso.";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Fechar conexão
+    $conn->close();
 }
-
-// Inserção de dados de livros
-$sql = "INSERT INTO livros (id_livro, titulo, ano_publicacao, id_autor) VALUES
-    (1, 'Livro 1', 2000, 1),
-    (2, 'Livro 2', 2010, 2),
-    (3, 'Livro 3', 2020, 3)";
-
-if ($conn->multi_query($sql) === TRUE) {
-    echo "Dados de livros inseridos com sucesso.<br>";
-} else {
-    echo "Erro ao inserir dados de livros: " . $conn->error;
-}
-
-// Inserção de dados de eventos
-$sql = "INSERT INTO eventos (id_evento, nome_evento, data) VALUES
-    (1, 'Evento 1', '2023-01-01'),
-    (2, 'Evento 2', '2023-02-15'),
-    (3, 'Evento 3', '2023-05-10')";
-
-if ($conn->multi_query($sql) === TRUE) {
-    echo "Dados de eventos inseridos com sucesso.<br>";
-} else {
-    echo "Erro ao inserir dados de eventos: " . $conn->error;
-}
-
-// Inserção de dados de participantes
-$sql = "INSERT INTO participantes (id_participante, id_evento, nome_participante) VALUES
-    (1, 1, 'Maxwell'),
-    (2, 1, 'Tiaguin'),
-    (3, 2, 'Tonhão'),
-    (4, 3, 'João Carlos')";
-
-if ($conn->multi_query($sql) === TRUE) {
-    echo "Dados de participantes inseridos com sucesso.<br>";
-} else {
-    echo "Erro ao inserir dados de participantes: " . $conn->error;
-}
-
-// Fecha a conexão
-$conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Cadastro de Livros e Autores</h2>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+ Título: <input type="text" name="titulo">
+ <br>
+ Ano de Publicação: <input type="text" name="ano_publicacao">
+ <br>
+ Nome do Autor: <input type="text" name="nome_autor">
+ <br><br>
+ <input type="submit" value="Cadastrar">
+</form>
+
+</body>
+</html>
