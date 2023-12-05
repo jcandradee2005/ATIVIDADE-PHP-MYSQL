c5f548aedd9b231041da9c02b31565ff15d4c952
jcandradee2005 <141359978+jcandradee2005@users.noreply.github.com>
2023-12-05 14:32:34 -0300

    Update produtoscategorias.php

--- inserção de dadosPHP/produtoscategorias.php
+++ inserção de dadosPHP/produtoscategorias.php
@@ -1,53 +1,76 @@
 <?php
-// nome: Maxwell
-// Conectar ao banco de dados (substitua pelos seus próprios dados)
-$servername = "seu_servidor";
-$username = "seu_usuario";
-$password = "sua_senha";
-$dbname = "seu_banco_de_dados";
+// conexão com o banco de dados
+$con = mysqli_connect("localhost", "root", "", "gerenciamento_produtos");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar a conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+if (mysqli_connect_errno()) {
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-// Função para adicionar produto e associar à categoria
-function adicionarProdutoCategoria($nomeProduto, $preco, $nomeCategoria) {
-    global $conn;
+// verificar se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Inserir produto
-    $sqlProduto = "INSERT INTO produtos (nome_produto, preco) VALUES ('$nomeProduto', '$preco')";
-    $conn->query($sqlProduto);
+ // inserir novo produto na tabela 'produtos'
+ $nome_produto = $_POST["nome_produto"];
+ $preco = $_POST["preco"];
 
-    // Obter o ID do produto recém-inserido
-    $idProduto = $conn->insert_id;
+ $sql = "INSERT INTO produtos (nome_produto, preco) VALUES ('$nome_produto', '$preco')";
 
-    // Verificar se a categoria já existe
-    $sqlCategoria = "SELECT id_categoria FROM categorias WHERE nome_categoria = '$nomeCategoria'";
-    $result = $conn->query($sqlCategoria);
+ if (mysqli_query($con, $sql)) {
+    $id_produto = mysqli_insert_id($con);
+    echo "New product registered successfully. Last inserted ID: " . $id_produto;
+ } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+ }
 
-    // Se a categoria não existir, inseri-la
-    if ($result->num_rows == 0) {
-        $sqlNovaCategoria = "INSERT INTO categorias (nome_categoria) VALUES ('$nomeCategoria')";
-        $conn->query($sqlNovaCategoria);
+ // associar categoria ao produto na tabela 'categorias'
+ $nome_categoria = $_POST["nome_categoria"];
 
-        // Obter o ID da categoria recém-inserida
-        $idCategoria = $conn->insert_id;
+ $sql = "SELECT id_categoria FROM categorias WHERE nome_categoria = '$nome_categoria'";
+ $result = mysqli_query($con, $sql);
+
+ if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $id_categoria = $row["id_categoria"];
+ } else {
+    $sql = "INSERT INTO categorias (nome_categoria) VALUES ('$nome_categoria')";
+
+    if (mysqli_query($con, $sql)) {
+        $id_categoria = mysqli_insert_id($con);
+        echo "New category registered successfully. Last inserted ID: " . $id_categoria;
     } else {
-        $row = $result->fetch_assoc();
-        $idCategoria = $row["id_categoria"];
+        echo "Error: " . $sql . "<br>" . mysqli_error($con);
     }
+ }
 
-    // Atualizar a tabela de produtos com a categoria associada
-    $sqlAtualizarProduto = "UPDATE produtos SET id_categoria = '$idCategoria' WHERE id_produto = '$idProduto'";
-    $conn->query($sqlAtualizarProduto);
+ // atribuir produto à categoria na tabela 'produtos_categorias'
+ $sql = "INSERT INTO produtos_categorias (id_produto, id_categoria) VALUES ('$id_produto', '$id_categoria')";
+
+ if (mysqli_query($con, $sql)) {
+    echo "Product " . $id_produto . " associated with category " . $id_categoria . " successfully.";
+ } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+ }
+
+ // fechar conexão
+ mysqli_close($con);
 }
-
-// Exemplo de uso
-adicionarProdutoCategoria("Produto B", 30.0, "Eletrônicos");
-
-// Fechar conexão
-$conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Registre um novo produto e atribua uma categoria</h2>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+ Nome do Produto: <input type="text" name="nome_produto">
+ <br>
+ Preço: <input type="text" name="preco">
+ <br>
+ Categoria: <input type="text" name="nome_categoria">
+ <br><br>
+ <input type="submit" value="Registrar">
+</form>
+
+</body>
+</html>
